545438e4b287ea6e8e856027f0fb34e2418fc4e0
vovantung <vovantungdt123@gmail.com>
2024-12-16 11:48:30 +0700

    121

--- src/main/java/com/example/app1/Api.java
+++ src/main/java/com/example/app1/Api.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Api {
     @GetMapping("/health-check")
     public String healthCheck(){
-        return "Võ Th Ngọc Uyên";
+        return "Phan Thị Xuyên";
     }
 }
