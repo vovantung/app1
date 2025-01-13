ed0240d4ae72c1ebdcc61e02b49a1bc9d3b2154b
vovantung <vovantungdt123@gmail.com>
2025-01-13 22:36:18 +0700

    cm163

--- src/main/java/com/example/app1/Api.java
+++ src/main/java/com/example/app1/Api.java
@@ -1,14 +1,17 @@
 package com.example.app1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping
 public class Api {
     @GetMapping("/health-check")
     public String healthCheck(){
+        log.info("Võ Thị Ngọc Uyên");
         return "Võ Thị Ngọc Uyên";
     }
 }
