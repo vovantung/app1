e2373a842ac3fcf312049ae6249ba3b391ddf8c6
vovantung <vovantungdt123@gmail.com>
2025-05-27 10:06:24 +0700

    cm201

--- src/main/java/com/example/app1/Api.java
+++ src/main/java/com/example/app1/Api.java
@@ -13,7 +13,7 @@ public class Api {
     @GetMapping("/health-check")
     public String healthCheck(){
         count++;
-        log.info("BBBBB");
-        return "BBBB";
+        log.info("QQQ");
+        return "QQQ";
     }
 }
