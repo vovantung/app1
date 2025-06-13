f8749655753ae66b38454a0e503978d9b3d57c02
vovantung <vovantungdt123@gmail.com>
2025-06-13 22:59:00 +0700

    cm396

--- src/main/java/com/example/app1/Api.java
+++ src/main/java/com/example/app1/Api.java
@@ -13,7 +13,7 @@ public class Api {
     @GetMapping("/test1")
     public String test1(){
         count++;
-        log.info("Võ Thị Ngọc Uyên");
-        return "Võ Thị Ngọc Uyên";
+        log.info("Võ Văn Tùng");
+        return "Võ Văn Tùng";
     }
 }
